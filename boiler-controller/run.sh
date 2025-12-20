0246084d50207ecda7ae618e7fa8fcb7e19578fb
euanjmackenzie-lgtm <euanjmackenzie@googlemail.com>
2025-12-20 12:52:05 +0000

    Create run.sh

---
+++ boiler-controller/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "Installing Boiler Controller package..."
+
+mkdir -p /config/packages/boiler
+cp -r /packages/boiler/* /config/packages/boiler/
+
+echo "Installation complete."
+echo "Restart Home Assistant to apply changes."
+
+sleep infinity
